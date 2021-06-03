35ac44b9e5796d6beb01d4ed7ae1964dac8b6e84
Anton Banov <anton_banov@epam.com>
2021-06-03 23:08:53 +0300

    for-if-cycles

--- js/script.js
+++ js/script.js
@@ -10,12 +10,16 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != '' && b != '') {
+    personalMovieDB.movies[a] = b; 
+    console.log('done');
+    } else {
+        console.log('error')
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
